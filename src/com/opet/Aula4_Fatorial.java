c9ca138519eb4257b81d74e9ef54769a7a1a424d
= <=>
2017-03-23 20:04:13 -0300

    Commit inicial

---
+++ src/com/opet/Aula4_Fatorial.java
@@ -0,0 +1,26 @@
+package com.opet;
+
+import com.opet.util.Reader;
+
+public class Aula4_Fatorial
+{
+
+    public static void main(String[] args) throws Exception
+    {
+        // TODO Auto-generated method stub
+        int numeroAnterior = 0;
+        int soma = 0;
+
+
+        System.out.print("Insira um número:");
+        int numeroUsuario = Reader.readInt();
+        for(int i=numeroUsuario; i>0; i--) {
+            numeroAnterior = numeroUsuario-(numeroUsuario-i)-1;
+            System.out.println(numeroUsuario+" * "+
+              numeroAnterior+": "+(numeroUsuario*numeroAnterior));
+            soma=soma+(numeroUsuario*numeroAnterior);
+        }
+        System.out.println("Fatorial de "+numeroUsuario+" é "+soma);
+    }
+
+}
